3b46077c9d0b78cd9c9466cb2bc26204a52973a9
ankita222 <ankitaverma.av222@gmail.com>
2017-05-08 16:02:23 +0530

    recommit 2

--- mCloud/UserPage/Dashboard.aspx.cs
+++ mCloud/UserPage/Dashboard.aspx.cs
@@ -16,6 +16,7 @@ namespace mCloud.UserPage
         protected void Page_Load(object sender, EventArgs e)
         {
             CreateTable();
+            loadDirectory();
         }
 
         public void CreateTable()
