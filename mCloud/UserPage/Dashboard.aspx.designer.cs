e71c6c6e78a50824da4b0759307af0b9362f12e2
khan0880jk <jamal_2000@ymail.com>
2017-05-09 10:49:23 +0530

    Dashboard 09-05-2017

--- mCloud/UserPage/Dashboard.aspx.designer.cs
+++ mCloud/UserPage/Dashboard.aspx.designer.cs
@@ -58,6 +58,15 @@ namespace mCloud.UserPage {
         protected global::System.Web.UI.WebControls.Repeater Repeater1;
         
         /// <summary>
+        /// Repeater2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater Repeater2;
+        
+        /// <summary>
         /// txtfolder control.
         /// </summary>
         /// <remarks>
